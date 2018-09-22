c9fc9b5d9577ff82dd21c3253d6f0f51fb7698a6
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-09-22 15:58:31 +0200

    implement timestamp API endpoint

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({ optionSuccessStatus: 200 }))
 app.use(express.static('public'));
 
@@ -11,7 +11,16 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/timestamp/:date*?", function (req, res) {
+  // check wether or not the date param exists, and if it does, checks if it is a unix timestamp in milliseconds or a UTC string
+  const date = req.params.date ? new Date(Number.isNaN(Number(req.params.date)) ? req.params.date : Number(req.params.date)) : new Date();
+  if (date.getTime()) res.json({ "unix": date.getTime(), "utc": date.toUTCString() });
+  else res.status(422).json({ "error": "Invalid Date" });
+})
+
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+module.exports = app;
